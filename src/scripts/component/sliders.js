0e4a633c640c724577a2c05461ccedb2c0194531
Hennadii Nikitenko <vankert353@gmail.com>
2019-07-06 03:26:00 +0300

    Завершена адаптация под мобайл

--- src/scripts/component/sliders.js
+++ src/scripts/component/sliders.js
@@ -90,7 +90,27 @@ module.exports = function () {
     dots: false,
     arrows: false,
     centerMode: true,
-    focusOnSelect: true
+    focusOnSelect: true,
+    responsive: [{
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
   });
 
   /**************************************************************** */
